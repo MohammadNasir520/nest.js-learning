d25478487a7deb200570fd1a68f08d4ecf1633e0
MohammadNasir520 <nasirahsan520@gmail.com>
2023-03-03 04:08:59 +0600

    dynamic route added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators';
 
 @Controller('book')
 export class BookController {
@@ -25,4 +26,15 @@ export class BookController {
   findAllBook(): string {
     return 'this will find all book';
   }
+
+  @Get('/findbookById/:id')
+  findBookById(@Param() params): string {
+    return `this will find a book by id of #${params.id}`;
+  }
 }
+
+// @Get(':id')
+// findOne(@Param() params): string {
+//   console.log(params.id);
+//   return `This action returns a #${params.id} cat`;
+// }
