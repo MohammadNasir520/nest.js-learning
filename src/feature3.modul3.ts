8fff9983382fce1c6dd350e7f3ecbc54d5ff828c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-03-05 00:31:36 +0600

    mongodb installed

--- src/feature3.modul3.ts
+++ src/feature3.modul3.ts
@@ -1,12 +1,42 @@
-import { Module } from '@nestjs/common';
+// import { MongoClient } from 'mongodb';
 
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-})
-export class Feature3 {
-  constructor() {
-    console.log('Feature3');
-  }
-}
+// export const connectToDatabase = async () => {
+//   const uri =
+//     'mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority'; // replace with your connection string
+//   const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+
+//   try {
+//     await client.connect();
+//     console.log('Connected to MongoDB Atlas');
+//     return client.db('mydatabase'); // replace with your database name
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// };
+
+// ----------------------------------------------------------------
+
+// import { Controller, Get } from '@nestjs/common';
+// import { connectToDatabase } from './mongo.connection';
+
+// @Controller()
+// export class MyController {
+//   private db;
+
+//   constructor() {
+//     connectToDatabase()
+//       .then(db => this.db = db)
+//       .catch(err => console.error(err));
+//   }
+
+//   @Get()
+//   async getData() {
+//     const collection = this.db.collection('mycollection'); // replace with your collection name
+//     const data = await collection.find().toArray();
+//     return data;
+//   }
+// }
