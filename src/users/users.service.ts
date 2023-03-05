d038f11edce2d79b8e96d2bb35d6024537e46e8c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-03-05 16:00:45 +0600

    collection  correcction

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,52 +1,52 @@
-import { Body, Param, Injectable } from '@nestjs/common';
+import { Param, Injectable } from '@nestjs/common';
 import { ObjectId } from 'mongodb';
 import { connectToDatabase } from './mongo.connection';
-
+import { User } from './data/user.dto';
 @Injectable()
 export class UsersServices {
   private db;
-
+  private usersCollection;
   constructor() {
     connectToDatabase()
-      .then((db) => (this.db = db))
+      .then((db) => {
+        this.db = db;
+        this.usersCollection = db.collection('users');
+      })
       .catch((err) => console.error(err));
   }
 
   // add user
-  async create(body): Promise<string> {
-    const user = await this.db.collection('users').insertOne(body);
+  async create(body: User): Promise<User> {
+    const user = await this.usersCollection.insertOne(body);
 
     return user;
   }
 
   // find all users
   async findAll() {
-    const users = await this.db.collection('users').find().toArray();
+    const users = await this.usersCollection.find().toArray();
     return users;
   }
 
   // find oneUsers by Id
   async findOne(id: string) {
-    const user = await this.db
-      .collection('users')
-      .findOne({ _id: new ObjectId(id) });
+    const user = await this.usersCollection.findOne({ _id: new ObjectId(id) });
     return user;
   }
   // update user
-  async update(@Param('id') id: string, @Body() body) {
-    const updateUser = await this.db
-      .collection('users')
-      .updateOne({ _id: new ObjectId(id) }, { $set: body });
-    const user = await this.db
-      .collection('users')
-      .findOne({ _id: new ObjectId(id) });
+  async update(@Param('id') id: string, body: User) {
+    const updateUser = await this.usersCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: body },
+    );
+    const user = await this.usersCollection.findOne({ _id: new ObjectId(id) });
     return { updateUser, user };
   }
 
   async deleteUser(@Param('id') id: string) {
-    const deleteUser = await this.db
-      .collection('users')
-      .deleteOne({ _id: new ObjectId(id) });
+    const deleteUser = await this.usersCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
     return deleteUser;
   }
 }
